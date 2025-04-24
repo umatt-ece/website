a4abf56458d33e88b2ef2e92f446fcca0e8cbec0
Ramatjyot Singh <ramatjyot13.ca@gmail.com>
2025-04-23 20:59:59 -0500

    fixed build err

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import UnderConstruction from '../Components/UnderConstruction.vue'
-import Contact from '../views/ContactView.vue'
 
 // Define routes for the application
 export const routes = [

11d91d7733ffe4c078a2b601b471bcdc3e8c09c1
Ramatjyot Singh <ramatjyot13.ca@gmail.com>
2025-08-14 01:50:47 -0500

    added debug output

--- src/views/JoinView.vue
+++ src/views/JoinView.vue
@@ -117,13 +117,20 @@ const submitForm = async () => {
       submissionDate: new Date().toLocaleString(),
       currentYear: new Date().getFullYear()
     };
-    
+
+    const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+    const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+    const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
+    console.log('Service ID:', serviceId);
+    console.log('Template ID:', templateId);
+    console.log('Public Key:', publicKey);
+
     // Send email using EmailJS
     await emailjs.send(
-      import.meta.env.VITE_SERVICE_ID,
-      import.meta.env.VITE_TEMPLATE_ID,
+      serviceId,
+      templateId,
       templateParams,
-      import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+      publicKey
     );
     
     // Success
